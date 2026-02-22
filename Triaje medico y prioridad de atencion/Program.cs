1a3dba17b8becc5c5b9c7a709e21679bc4de7103
dschaym <dschay@gmail.com>
2026-02-21 22:09:52 -0600

    Agregue validaciones

--- Triaje medico y prioridad de atencion/Program.cs
+++ Triaje medico y prioridad de atencion/Program.cs
@@ -16,3 +16,34 @@ Console.WriteLine("Nivel de dolor (0-10)");
 int nivelDolor = int.Parse(Console.ReadLine());
 Console.WriteLine("Presion sistolica (50-200 mmHg)");
 int presionS = int.Parse(Console.ReadLine());
+
+if (tipoAtencion < 1 || tipoAtencion > 4)
+{
+    Console.WriteLine("Tipo de atencion no valido");
+    return;
+}
+else  if (edad < 0 || edad > 120)
+{
+    Console.WriteLine("Edad no valida");
+    return;
+}
+else if (temperatura < 30 || temperatura > 45)
+{
+    Console.WriteLine("Temperatura no valida");
+    return;
+}
+else if (saturacionO < 80 || saturacionO > 100)
+{
+    Console.WriteLine("Saturacion de oxigeno no valida");
+    return;
+}
+else if (nivelDolor < 0 || nivelDolor > 10)
+{
+    Console.WriteLine("Nivel de dolor no valido");
+    return;
+}
+else if (presionS < 50 || presionS > 200)
+{
+    Console.WriteLine("Presion sistolica no valida");
+    return;
+}
